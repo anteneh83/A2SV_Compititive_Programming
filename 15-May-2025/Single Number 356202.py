efed1e3d16069f0742d4f5cc5b0727674a0e0f8d
Anti G <128841483+anteneh83@users.noreply.github.com>
2025-05-15 04:56:47 -0900

    Created 'Single Number' by A2SV Hub

---
+++ 15-May-2025/Single Number 356202.py
@@ -0,0 +1,9 @@
+# Problem: Single Number - https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        ans=0
+        for num in nums:
+            ans = ans ^ num
+        # print(ans)
+        return ans
