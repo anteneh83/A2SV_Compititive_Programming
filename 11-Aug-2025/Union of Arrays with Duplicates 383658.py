64328ec260626088ae4c9a630fb4a5d32bd2bd6b
Anti G <128841483+anteneh83@users.noreply.github.com>
2025-08-11 10:18:50 -0900

    Created 'Union of Arrays with Duplicates' by A2SV Hub

---
+++ 11-Aug-2025/Union of Arrays with Duplicates 383658.py
@@ -0,0 +1,8 @@
+# Problem: Union of Arrays with Duplicates - https://practice.geeksforgeeks.org/problems/union-of-two-arrays3538/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+
+class Solution:    
+    def findUnion(self, a, b):
+        ans = set(a)
+        for l in b:
+            ans.add(l)
+        return ans
