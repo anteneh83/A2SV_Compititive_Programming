8dbbffcee10a762a32a3091a688cc36a3519e480
Anti G <128841483+anteneh83@users.noreply.github.com>
2025-08-12 22:54:32 -0900

    Created 'Heaters' by A2SV Hub

---
+++ 13-Aug-2025/Heaters 384507.py
@@ -0,0 +1,20 @@
+# Problem: Heaters - https://leetcode.com/problems/heaters/
+
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        # print(bisect.bisect_left(heaters, 4))
+        def get_max(house):
+            pos = bisect.bisect_left(heaters, house)
+            left_rad = float('inf') if pos == 0 else house - heaters[pos-1]
+            right_rad = float('inf') if pos == len(heaters) else heaters[pos] - house
+
+            return min(left_rad, right_rad)
+
+        ans = 0
+        for house in houses:
+            ans = max(ans, get_max(house))
+        return ans
+
+            
